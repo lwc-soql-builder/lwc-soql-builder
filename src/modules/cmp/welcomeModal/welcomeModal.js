6e907268e46fd62e14d29433cd2c7e36e2bcd357
Atsuhiko Kimura <atskimura@gmail.com>
2020-03-29 16:17:54 +0900

    Fix location origin not displaying

--- src/modules/cmp/welcomeModal/welcomeModal.js
+++ src/modules/cmp/welcomeModal/welcomeModal.js
@@ -2,6 +2,10 @@ import { LightningElement } from 'lwc';
 import salesforce from '../../service/salesforceService';
 
 export default class WelcomeModal extends LightningElement {
+    get locationOrigin() {
+        return salesforce.locationOrigin;
+    }
+
     loginProduction() {
         salesforce.login('https://login.salesforce.com');
     }
